6a66cf5b0613eefbd46b1f178df074cdf826a3a2
sri hari <srihari357@OUTLOOK.COM>
2026-02-16 17:51:31 -0600

    Remove CORS dependency

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 import datetime
 import json
 import os
 
-app = Flask(__name__)
-CORS(app)
+#app = Flask(__name__)
+#CORS(app)
 
 # Restaurant Menu
 MENU = {
